8ca43f6e26a3e8132394a80c2f7d245dccd78f71
sakitam-fdd <smilefdd@gmail.com>
2018-06-04 17:50:54 +0800

    del tpl

--- template/src/build/rollup-dev-config.js
+++
@@ -1,8 +0,0 @@
-const { input, output } = require('./rollup-base-config')[0];
-{{#scss}}
-const {cssPlugins} = require('./helper');
-input.plugins.splice(2, 0, cssPlugins());
-{{/scss}}
-module.exports = Object.assign({
-  plugins: []
-}, input, {output});
--- template/src/src/index.js
+++
@@ -1,3 +0,0 @@
-import * as maptalks from 'maptalks';
-
-export class HelloLayer extends maptalks.Layer {}
